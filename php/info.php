88792e25ccf4f33276d1daf4659a3f9e27b04dd5
LDubina <ldubina@yandex.ru>
2020-05-11 14:43:55 +0300

    Через промежуточное сохранение

--- php/info.php
+++ php/info.php
@@ -1,5 +1,16 @@
 <?php
-$Str_val='ggg';
+/**
+ * Обязательный комментарий к файлу
+ * PHP Version 7
+ *
+ * @category Demo
+ * @package  TutsPlus_Demo
+ * @author   Tom McFarlin <tom@tommcfarlin.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://is.gd/dq0DhO
+ * @since    1.0.0
+ */
+ $Str_val='ggg';
 echo $Str_val.'Привет';
 // Показывать всю информацию, по умолчанию INFO_ALL
 phpinfo();
